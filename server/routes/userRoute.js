c40f0e6d1b33500f799c9a54d5a6c1e0772af2bb
Badar Us Salam <badaronline86@gmail.com>
2023-11-30 23:46:50 +0100

    Login functionality and route created

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -20,4 +20,28 @@ router.post("/register", (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.find({ email, password });
+    if (user.length > 0) {
+      const currentUser = {
+        name: user[0].name,
+        email: user[0].email,
+        isAdmin: user[0].isAdmin,
+        _id: user[0].Id,
+      };
+      res.status(200).send(currentUser);
+    } else {
+      res.status(400).json({
+        message: "Login Failed",
+      });
+    }
+  } catch (error) {
+    res.status(404).json({
+      message: "Something went Wrong ",
+    });
+  }
+});
+
 export default router;
